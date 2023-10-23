0315f99b66a9977042cd07f04ef52a570b5ecfa0
jullev <setiyawanjullev@gmail.com>
2023-10-23 14:49:37 +0700

    modelProfile

---
+++ app/src/main/java/com/jti/aplikasihijau/Model/ModelProfile.java
@@ -0,0 +1,40 @@
+package com.jti.aplikasihijau.Model;
+
+public class ModelProfile {
+    String firstName;
+    String lastName;
+    String Email;
+    String imageProfil;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getImageProfil() {
+        return imageProfil;
+    }
+
+    public void setImageProfil(String imageProfil) {
+        this.imageProfil = imageProfil;
+    }
+}
